d66d877988515a86fe49c6169cded6738c5f88af
Christopher <cjszk@hotmail.com>
2018-05-03 12:13:46 -0700

    redone landing-page

--- client-side/src/components/landing-page.js
+++ client-side/src/components/landing-page.js
@@ -1,50 +1,133 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import imageDueTasks from '../styles/images/due-tasks.jpg'
-import imageCalendar from '../styles/images/calendar.jpg'
-import imageWidgets from '../styles/images/widgets.jpg'
 
-export function LandingPage (props) {
+import imageNotifications from '../styles/images/notifications.jpg';
+import imageCalendar from '../styles/images/calendar.jpg';
+import imageWidgets from '../styles/images/widgets.jpg';
+import imageTasks from '../styles/images/tasks.jpg';
+import imageHeadWay from '../styles/images/headway.jpg';
 
-    return (
-            <div className="landing-page">
-                <div className="landing-page__div-first">
-                    <img className="landing-page__image" alt="Computer"></img>
-                    <h2 className="landing-page__header">Make productivity simple</h2>
+import svgCalendar from '../styles/images/SVG/calendar.svg';
+import svgMusic from '../styles/images/SVG/music.svg';
+import svgAlarm from '../styles/images/SVG/alarm.svg';
+import svgFiletext from '../styles/images/SVG/filetext.svg';
+
+export class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTab: 0,
+            clicked: false
+        }
+    }
+
+    changeTab() {
+        if (this.state.currentTab < 4) {
+            this.setState({currentTab: this.state.currentTab + 1});
+        } else {
+            this.setState({currentTab: 0});
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+
+        let bindThis = this;
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+        if (!this.state.clicked) {
+            this.interval = setInterval(function() {
+                bindThis.changeTab();
+            }, 5000)
+        }
+
+        let tab;
+
+        if (this.state.currentTab === 0) {
+            tab = (
+                <div>
+                    <img src={imageHeadWay} className="landing-page__main__image landing-page__main__image__0" alt="Computer"></img>
+                    <h2 className="landing-page__main__header">Make productivity simple</h2>
                 </div>
-                <div className="landing-page__div-second">
-                    <h3 className="landing-page__secondary-header"></h3>
-                    <p className="landing-page__paragraph"></p>
+            )
+        } else if (this.state.currentTab === 1) {
+            tab = (
+                <div>
+                    <img src={imageCalendar} className="landing-page__main__image landing-page__main__image__1" alt="Computer"></img>
+                    <h2 className="landing-page__main__header">Click-and-go Simple Calendars</h2>
                 </div>
-                <div className="landing-page__div-third">
-                    <ul className="landing-page__about">
-                        <p className="landing-page__about__message">Made with busy computer users in mind!</p>
-                        <li className="landing-page__about__li">
-                        <p className="landing-page__about__li__paragraph">Gain access to a calendar in which you can manage your daily tasks.</p>
-                        <img class="landing-page__about__li__image imageDueTasks" src={imageCalendar} alt="Calendar Tab"/>
-                        </li>
-                        <li className="landing-page__about__li">
-                            <p className="landing-page__about__li__paragraph">Be alerted of past due tasks and tasks due on the current day.</p>
-                            <img class="landing-page__about__li__image imageDueTasks" src={imageDueTasks} alt="Due Tasks Tab"/>
-                        </li >
-                        <li className="landing-page__about__li">
-                        <p className="landing-page__about__li__paragraph">Gain access to multiple widgets to assist you in being productive.</p>
-                        <img class="landing-page__about__li__image imageDueTasks" src={imageWidgets} alt="Calendar Tab"/>
-                        </li>
-                    </ul>
+            )
+        } else if (this.state.currentTab === 2) {
+            tab = (
+                <div>
+                    <img src={imageTasks} className="landing-page__main__image landing-page__main__image__2" alt="Computer"></img>
+                    <h2 className="landing-page__main__header">Planning is only a few clicks away</h2>
                 </div>
-                <div className="landing-page__div-fourth">
-                    <h3 className="landing-page__secondary-header"></h3>
-                    <ol className="landing-page__list"> 
-                        {/* <li className="landing-page__list-item"></li>
-                        <li className="landing-page__list-item"></li>
-                        <li className="landing-page__list-item"></li>
-                        <li className="landing-page__list-item"></li> */}
-                    </ol>
-                    <p className="landing-page__div-fourth__paragraph">Sign up now! <a href="/login">Create a new account</a></p>
+            )
+        } else if (this.state.currentTab === 3) {
+            tab = (
+                <div>
+                    <img src={imageNotifications} className="landing-page__main__image landing-page__main__image__3" alt="Computer"></img>
+                    <h2 className="landing-page__main__header">Never forget anything anymore</h2>
+                </div>
+            )
+        } else if (this.state.currentTab === 4) {
+            tab = (
+                <div>
+                    <img src={imageWidgets} className="landing-page__main__image landing-page__main__image__4" alt="Computer"></img>
+                    <h2 className="landing-page__main__header">Productivity should be enjoyable</h2>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <div className="landing-page">
+                    <div className="landing-page__main">
+                        {/* <button className="landing-page__main__left-button">&#8592;</button> */}
+                        {tab}
+                        {/* <button className="landing-page__main__right-button">&#8594;</button> */}
+                    </div>
+                    <div className="landing-page__tabs">
+                        <ul className="landing-page__tabs__ul">
+                            <li onClick={() => this.setState({currentTab: 1, clicked: true})} className="landing-page__tabs__li">
+                                <a className="landing-page__tabs__li__select">
+                                    <img className="landing-page__tabs__li__image" src={svgCalendar} className="" alt="Calendar"></img>
+                                    <span className="landing-page__tabs__li__text">Intuitively Simple Calendars</span>
+                                </a>
+                            </li>
+                            <li onClick={() => this.setState({currentTab: 2, clicked: true})} className="landing-page__tabs__li">
+                                <a  className="landing-page__tabs__li__select">
+                                    <img className="landing-page__tabs__li__image" src={svgFiletext} className="" alt="Calendar"></img>
+                                    <span className="landing-page__tabs__li__text">Fast-tracked Planning</span>
+                                </a>
+                            </li>
+                            <li onClick={() => this.setState({currentTab: 3, clicked: true})} className="landing-page__tabs__li">
+                                <a  className="landing-page__tabs__li__select">
+                                    <img className="landing-page__tabs__li__image" src={svgAlarm} className="" alt="Calendar"></img>
+                                    <span className="landing-page__tabs__li__text">Responsive Notifications</span>
+                                </a>
+                            </li>
+                            <li onClick={() => this.setState({currentTab: 4, clicked: true})} className="landing-page__tabs__li">
+                                <a className="landing-page__tabs__li__select">
+                                    <img className="landing-page__tabs__li__image" src={svgMusic} className="" alt="Calendar"></img>
+                                    <span className="landing-page__tabs__li__text">Excitingly Productive Widgets</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="signup-now">
+                    <p className="signup-now__message">Sign up now! <a href="/login">Create a new account</a></p>
                 </div>
             </div>
-        )
+            )
+    }
 
 }
 
