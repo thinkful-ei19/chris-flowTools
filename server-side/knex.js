b74a0286f18279256b3de631c5078f15acb49499
Christopher <cjszk@hotmail.com>
2018-05-03 12:39:48 -0700

    elephantSQL set up

--- server-side/knex.js
+++ server-side/knex.js
@@ -1,7 +1,7 @@
 const knexConfig = {
     development: {
       client: 'pg',
-      connection: process.env.DATABASE_URL || 'postgres://chris:cjdrnch65@localhost/flowTools',
+      connection: process.env.DATABASE_URL || '	postgres://pmpyfkhc:B0wzq840WeSgXurlQ36KuIXMKa4yvv_o@pellefant.db.elephantsql.com:5432/pmpyfkhc',
       debug: true, // http://knexjs.org/#Installation-debug
       pool: {min : 1 , max : 2}
     },
